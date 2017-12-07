74b2fe7ee7c1f8f73f6b4d3fdc4f6730b91f6fef
杨文杰 <ywj@geekniu.com>
2017-12-07 18:52:04 +0800

    新增自定义的 map 方法

--- 1.js
+++ 1.js
@@ -1,2 +1,15 @@
-console.log(1);
-console.log('hello world!');
+Array.prototype._map = function(callback) {
+    var index = 0;
+    var item;
+    var newArr = [];
+    while(item = this[index]) {
+        newArr.push(callback(item, index++, this));
+    }
+    return newArr;
+}
+
+const arr = [10, 10, 10, 10];
+
+const newArr = arr._map((item, index, arr) => item * index);
+
+console.log(newArr);
