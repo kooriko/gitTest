587ebe115ba8504701cfdedd5804fe6adcd4978e
杨文杰 <ywj@geekniu.com>
2017-12-07 19:14:38 +0800

    新增 find 自定义方法

--- 1.js
+++ 1.js
@@ -8,8 +8,29 @@ Array.prototype._map = function(callback) {
     return newArr;
 }
 
-const arr = [10, 10, 10, 10];
+Array.prototype._find = function(callback) {
+    var index = 0;
+    var item;
+    while(item = this[index]) {
+        var result = callback(item, index++, this);
+        if(result) {
+            return item;
+        }
+    }
+}
+
+const arr = [{
+    a: 1,
+    b: 2,
+}, {
+    a: 3,
+    b: 4,
+}, {
+    a: 5,
+    b: 6,
+}];
 
 const newArr = arr._map((item, index, arr) => item * index);
 
-console.log(newArr);
+const fined = arr._find((item, index, arr) => item.a === 5);
+console.log(fined);
